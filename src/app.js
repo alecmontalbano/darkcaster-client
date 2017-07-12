b8d5d150d3d93d955bfaf13e4748b68556387c65
Alec Montalbano <alec.montalbano@gmail.com>
2017-07-11 20:09:33 -0400

    created the weather service that is currently mocked using our mock weather data. Change the MainController to use the service to obtain said data

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
 /*jshint esversion:6*/
 const angular = require('angular');
-const weatherData = require('../mocks/weather.json');
 
+
+
+//services
+const WeatherService = require('./services/weather.service');
 
 //create our application
 angular.module('darkcaster-client', []); //setter syntax
 
 angular.module('darkcaster-client') //getter syntax
-       .controller('MainController', MainController);
+       .controller('MainController', MainController)
+       .factory('WeatherService', WeatherService);
 
-MainController.$inject = []; //magic for us
+MainController.$inject = ['WeatherService']; //magic for us
 
-function MainController(){
+function MainController(weather){
   this.message = 'hello from angular';
-  this.weatherData = weatherData.currently;
+  this.weatherData = weather.getCurrently();
 }
